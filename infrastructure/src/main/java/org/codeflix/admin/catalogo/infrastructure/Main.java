9a81b2e19a2f137891856898d3e6bd8b3e8c60f2
marciorfc <marcio.carneiro@serpro.gov.br>
2025-07-04 10:10:16 -0300

    Spring boot initial infrastructure config, application properties and mysql connection

--- infrastructure/src/main/java/org/codeflix/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/org/codeflix/admin/catalogo/infrastructure/Main.java
@@ -1,15 +1,21 @@
 package org.codeflix.admin.catalogo.infrastructure;
 
 import org.codeflix.admin.catalogo.application.UseCase;
+import org.codeflix.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
         System.out.println("Hello and welcome!");
-
        // System.out.println(new UseCase().execute());
     }
 }
---
+++ infrastructure/src/main/java/org/codeflix/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package org.codeflix.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.codeflix.admin.catalogo")
+public class WebServerConfig {
+}
